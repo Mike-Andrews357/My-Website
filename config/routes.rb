ffb5e1f44287c26f7573052b61aeb796125118fa
Michael Andrews <mike.andrews357@gmail.com>
2019-02-01 11:10:47 -0500

    comments and mail setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
-  resources :posts
+  
   devise_for :users, controllers: { registrations: 'user_registrations/registrations'}
   resources :template_ones
 
-  root to: 'index', to: "template_ones#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "template_ones#index"
+
+  resources :posts do
+    resources :comments
+  end
+  
 end
